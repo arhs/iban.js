17e4d61db4989b392e52cea909d45638eceb7ef1
Laurent VB <laurent@e-founders.com>
2018-02-07 11:00:27 +0100

    Include missing countries from the 2017 updates - Belarus - Iraq - Jordan - Qatar - Seychelles - El Salvador - Ukraine

--- iban.js
+++ iban.js
@@ -225,6 +225,7 @@
     addSpecification(new Specification("BG", 22, "U04F04F02A08",       "BG80BNBG96611020345678"));
     addSpecification(new Specification("BH", 22, "U04A14",             "BH67BMAG00001299123456"));
     addSpecification(new Specification("BR", 29, "F08F05F10U01A01",    "BR9700360305000010009795493P1"));
+    addSpecification(new Specification("BY", 28, "A04F04A16",          "BY13NBRB3600900000002Z00AB00"));
     addSpecification(new Specification("CH", 21, "F05A12",             "CH9300762011623852957"));
     addSpecification(new Specification("CR", 22, "F04F14",             "CR72012300000171549015"));
     addSpecification(new Specification("CY", 28, "F03F05A16",          "CY17002001280000001200527600"));
@@ -249,6 +250,8 @@
     addSpecification(new Specification("IL", 23, "F03F03F13",          "IL620108000000099999999"));
     addSpecification(new Specification("IS", 26, "F04F02F06F10",       "IS140159260076545510730339"));
     addSpecification(new Specification("IT", 27, "U01F05F05A12",       "IT60X0542811101000000123456"));
+    addSpecification(new Specification("IQ", 23, "U04F03A12",          "IQ98NBIQ850123456789012"));
+    addSpecification(new Specification("JO", 30, "A04F22",             "JO15AAAA1234567890123456789012"));
     addSpecification(new Specification("KW", 30, "U04A22",             "KW81CBKU0000000000001234560101"));
     addSpecification(new Specification("KZ", 20, "F03A13",             "KZ86125KZT5004100100"));
     addSpecification(new Specification("LB", 28, "F04A20",             "LB62099900000001001901229114"));
@@ -270,19 +273,26 @@
     addSpecification(new Specification("PL", 28, "F08F16",             "PL61109010140000071219812874"));
     addSpecification(new Specification("PS", 29, "U04A21",             "PS92PALS000000000400123456702"));
     addSpecification(new Specification("PT", 25, "F04F04F11F02",       "PT50000201231234567890154"));
+    addSpecification(new Specification("QA", 29, "U04A21",             "QA30AAAA123456789012345678901"));
     addSpecification(new Specification("RO", 24, "U04A16",             "RO49AAAA1B31007593840000"));
     addSpecification(new Specification("RS", 22, "F03F13F02",          "RS35260005601001611379"));
     addSpecification(new Specification("SA", 24, "F02A18",             "SA0380000000608010167519"));
+    addSpecification(new Specification("SC", 31, "U04F04F16U03",       "SC18SSCB11010000000000001497USD"));
     addSpecification(new Specification("SE", 24, "F03F16F01",          "SE4550000000058398257466"));
     addSpecification(new Specification("SI", 19, "F05F08F02",          "SI56263300012039086"));
     addSpecification(new Specification("SK", 24, "F04F06F10",          "SK3112000000198742637541"));
     addSpecification(new Specification("SM", 27, "U01F05F05A12",       "SM86U0322509800000000270100"));
     addSpecification(new Specification("ST", 25, "F08F11F02",          "ST68000100010051845310112"));
+    addSpecification(new Specification("SV", 28, "U04F20",             "SV62CENR00000000000000700025"));
     addSpecification(new Specification("TL", 23, "F03F14F02",          "TL380080012345678910157"));
     addSpecification(new Specification("TN", 24, "F02F03F13F02",       "TN5910006035183598478831"));
     addSpecification(new Specification("TR", 26, "F05F01A16",          "TR330006100519786457841326"));
+    addSpecification(new Specification("UA", 29, "F25",                "UA511234567890123456789012345"));
     addSpecification(new Specification("VG", 24, "U04F16",             "VG96VPVG0000012345678901"));
     addSpecification(new Specification("XK", 20, "F04F10F02",          "XK051212012345678906"));
+
+
+    // The following countries are not included in the official IBAN registry but use the IBAN specification
 
     // Angola
     addSpecification(new Specification("AO", 25, "F21",                "AO69123456789012345678901"));
@@ -302,22 +312,14 @@
     addSpecification(new Specification("DZ", 24, "F20",                "DZ8612345678901234567890"));
     // Iran
     addSpecification(new Specification("IR", 26, "F22",                "IR861234568790123456789012"));
-    // Jordan
-    addSpecification(new Specification("JO", 30, "A04F22",             "JO15AAAA1234567890123456789012"));
     // Madagascar
     addSpecification(new Specification("MG", 27, "F23",                "MG1812345678901234567890123"));
     // Mali
     addSpecification(new Specification("ML", 28, "U01F23",             "ML15A12345678901234567890123"));
     // Mozambique
     addSpecification(new Specification("MZ", 25, "F21",                "MZ25123456789012345678901"));
-    // Quatar
-    addSpecification(new Specification("QA", 29, "U04A21",             "QA30AAAA123456789012345678901"));
     // Senegal
     addSpecification(new Specification("SN", 28, "U01F23",             "SN52A12345678901234567890123"));
-    // Ukraine
-    addSpecification(new Specification("UA", 29, "F25",                "UA511234567890123456789012345"));
-    // Seychelles
-    addSpecification(new Specification("SC", 31, "U04F04F16U03",       "SC18SSCB11010000000000001497USD"));
 
     var NON_ALPHANUM = /[^a-zA-Z0-9]/g,
         EVERY_FOUR_CHARS =/(.{4})(?!$)/g;
